c1335e78e3db0f683e3f2313b5b38a3147a85350
Korban88 <korban88@gmail.com>
2025-08-17 17:24:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils import executor
 
 from config import settings
-from handlers import register_handlers            # хэндлеры остаются тут
-from scanner import scanner_loop                  # сканер вынесен в scanner.py
+from handlers import register_handlers            # хэндлеры
+from scanner import scanner_loop                  # фоновый цикл сканера
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("nftbot")
@@ -16,7 +16,7 @@ logger = logging.getLogger("nftbot")
 
 async def on_startup(dp: Dispatcher):
     logger.info("Starting NFT bot (Iteration 1 / Step 1)...")
-    # Фоновый цикл сканера
+    # Запускаем фоновый сканер
     dp.loop.create_task(scanner_loop())
 
 
@@ -26,7 +26,6 @@ def main():
 
     register_handlers(dp)
 
-    # Поллинг
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
 
 
